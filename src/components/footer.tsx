41e86bed4b8eaa67804b94a4258d1a5bf6485085
QuocHuy <quochuy@phutungb2b.com>
2026-09-22 17:36:05 +0700

    State và Events với TypeScript

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <div>© {new Date().getFullYear()} VibeCoding • Đỗ Quốc Huy</div>
+    </footer>
+  );
+};
+
+export default Footer;
